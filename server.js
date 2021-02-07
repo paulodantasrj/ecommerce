3fcc7bcaefdde8d8bdcbc97e16c88b93ebd39c0f
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-02-07 13:16:00 -0300

    initial commit - setup

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  }),
+);
+
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+  (error) => {
+    if (error) throw error;
+    console.log('mongo conectado');
+  },
+);
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'oi' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('server rodando');
+});
